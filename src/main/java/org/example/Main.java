b0aba2e427977575a986266a90e81c05a2d6b227
Pariente <neodante12@gmail.com>
2024-11-03 17:18:07 -0700

    Esta sería la version final que ya se comunica con el endpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,30 +11,8 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        // Define la ruta del WSDL y la ruta donde se generarán las clases
-        String wsdlUrl = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL";
-        String outputDir = "src/main/java/";
 
-        // Configura el comando para wsimport
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("wsimport", "-s", outputDir, wsdlUrl);
-
-        // Establece el directorio de trabajo donde se ejecutará el comando
-        processBuilder.directory(new File("E:\\Java8intentoBorrar")); // Cambia a tu directorio base si es necesario
-
-        try {
-            Process process = processBuilder.start();
-            int exitCode = process.waitFor(); // Espera a que el proceso termine
-
-            if (exitCode == 0) {
-                System.out.println("Clases generadas con éxito.");
-                interactWithApi();
-            } else {
-                System.err.println("Error al ejecutar wsimport. Código de salida: " + exitCode);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        interactWithApi();
     }
     private static void interactWithApi() {
         try {
@@ -48,7 +26,7 @@ public class Main {
             // Obtiene el puerto para interactuar con el servicio
             NumberConversionSoapType port = service.getPort(NumberConversionSoapType.class);
 
-            String response = port.numberToWords(BigInteger.valueOf(1111111)); // Llama al método con la solicitud
+            String response = port.numberToWords(BigInteger.valueOf(666)); // Llama al método con la solicitud
             System.out.println("Resultado: " + response);
 
         } catch (Exception e) {
